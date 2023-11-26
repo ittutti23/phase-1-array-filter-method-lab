949bbdb6781eb017fb698cebb9396f3bea4de7a8
Peyton <1209942pl@gmail.com>
2023-11-26 17:14:25 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, search) {
+    const matchingDrivers = [];
+  
+    drivers.forEach(driver => {
+      const lowercaseDriver = driver.toLowerCase();
+      const lowercaseSearch = search.toLowerCase();
+  
+      if (lowercaseDriver.includes(lowercaseSearch)) {
+        matchingDrivers.push(driver);
+      }
+    });
+  
+    return matchingDrivers;
+  }
